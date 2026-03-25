7e09e079cfc4b1291f95b47f23db84e5a2c7daab
Winnie Shi <wshi@macalester.edu>
2026-03-25 15:32:43 -0500

    add package, map, tileset, and player

---
+++ Assets/Tileset/House.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="House" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="House.png" width="155" height="133"/>
+</tileset>
---
+++ Assets/Tileset/cave_entrance.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="cave_entrance" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <image source="cave_entrance.png" width="192" height="128"/>
+</tileset>
