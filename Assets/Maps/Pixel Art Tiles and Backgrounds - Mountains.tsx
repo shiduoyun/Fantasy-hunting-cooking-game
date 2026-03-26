20be9eaea3e82c4ef9aced27f8e39d5b560ee963
Winnie Shi <wshi@macalester.edu>
2026-03-26 09:49:31 -0500

    upload background and rigidbody for A

--- Assets/Maps/Pixel Art Tiles and Backgrounds - Mountains.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="Pixel Art Tiles and Backgrounds - Mountains" tilewidth="32" tileheight="32" tilecount="144" columns="16">
- <image source="../Tileset/Pixel Art Tiles and Backgrounds - Mountains.png" width="512" height="288"/>
-</tileset>
